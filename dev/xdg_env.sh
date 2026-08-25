220db024ceefa716c2f8396e7e0422564b5193f0
Alex Candido <acs11@cin.ufpe.br>
2026-08-25 10:27:43 -0300

    [dev] add extra environment variables setting XDG compliant paths to other programs

--- dev/xdg_env.sh
+++ dev/xdg_env.sh
@@ -11,21 +11,32 @@ export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
 export NPM_CONFIG_PREFIX="$HOME/.local"
+export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
+export PNPM_HOME="$XDG_DATA_HOME/pnpm"
+export BUN_INSTALL="$XDG_DATA_HOME/bun"
+
 
 # --- Elixir / Erlang (.hex) ---
 export HEX_HOME="$XDG_DATA_HOME/hex"
 export MIX_HOME="$XDG_DATA_HOME/mix"
 export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 
-# --- GitHub Copilot ---
-# Warning: COPILOT_HOME mixes config and state data. 
-export COPILOT_HOME="$XDG_DATA_HOME/copilot"
-
 # --- NVIDIA / CUDA ---
 export __GL_SHADER_DISK_CACHE_PATH="$XDG_CACHE_HOME/nv"
 export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
 
+# --- AI STUF
+export COPILOT_HOME="$XDG_DATA_HOME/copilot"
+export PI_CODING_AGENT_DIR="$XDG_CONFIG_HOME/pi"
+export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/claude"
+export OLLAMA_MODELS="$XDG_DATA_HOME/ollama/models"
+export HF_HOME="$XDG_DATA_HOME/huggingface"
 
+# --- MISC LANGUAGES
+export PYTHONUSERBASE="$XDG_DATA_HOME/python"
+export GOPATH="$XDG_DATA_HOME/go"
 # --- Other Tools ---
 export UV_PYTHON_PREFERENCE="system"
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+export AWS_SHARED_CREDENTIALS_FILE="$XDG_CONFIG_HOME/aws/credentials"
+export AWS_CONFIG_FILE="$XDG_CONFIG_HOME/aws/config"
